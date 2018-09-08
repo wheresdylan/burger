7fb06b24da95d1513ccdf9d74a1e660968994916
Dylan Marini <dylan.marini@g.austincc.edu>
2018-09-07 20:58:19 -0500

    fixing

--- config/connection.js
+++ config/connection.js
@@ -14,6 +14,7 @@ if (process.env.JAWSDB_URL){
 }else {
   connection = mysql.createConnection({
     host: "localhost",
+    port: 8889,
     user: "root",
     password: "root",
     database: "burgers_db"
