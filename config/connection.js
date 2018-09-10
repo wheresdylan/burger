582c88506572b04e73c32c196e29dd0de2c6cf9f
Dylan Marini <dylan.marini@g.austincc.edu>
2018-09-10 14:56:59 -0500

    getting deployment to work

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,11 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
 
 if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else {
-  connection = mysql.createConnection({
+  var connection = mysql.createConnection({
     host: "localhost",
     port: 8889,
     user: "root",
